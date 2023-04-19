e564a095a8442e966168785aea0268a898a8909a
Jao42 <joaovictormedeiros41@gmail.com>
2023-04-19 16:17:04 -0300

    renomeando variaveis limpar_dados

--- limpar_dados.py
+++ limpar_dados.py
@@ -4,7 +4,7 @@ import json
 from pandas import read_html
 
 
-def pegarDetalhar(link):
+def pegar_detalhar(link):
 
   det = read_html(link)[0]
   for i in range(len(det['Nota Obtida'])):
@@ -15,23 +15,25 @@ def pegarDetalhar(link):
 
 def sanitizar_saida(arq, session):
 
+  INDICES_NOTAS_PADRAO = [7, 9, 11, 13, 15, 18]
+  LABELS_NOTAS_TABLE = ['E1', 'E2', 'E3', 'E4', 'MD', 'CONCEITO']
+
   file = open(arq, 'r')
   html_file = file.read()
   file.close()
 
   soup = BeautifulSoup(html_file, 'html.parser')
-  soup = soup.tbody
-  materias_html = soup.find_all('tr')
+  corpo_tabela_soup = soup.tbody
+  materias_html = corpo_tabela_soup.find_all('tr')
 
   materias = {}
 
-  labels = ['E1', 'E2', 'E3', 'E4', 'MD', 'CONCEITO']
   for i in range(len(materias_html)):
 
     soup = BeautifulSoup(str(materias_html[i]), 'html.parser')
-    materias_html_td = soup.find_all('td')
+    materia_tds = soup.find_all('td')
 
-    disciplina_soup = materias_html_td[1]
+    disciplina_soup = materia_tds[1]
     disciplina = disciplina_soup.get_text().replace('\n', '').strip().replace('  ', '')
     materias[disciplina] = {}
     materia_medias_ref = [materias_html_td[7], materias_html_td[9], materias_html_td[11], materias_html_td[13], materias_html_td[15], materias_html_td[18]]
@@ -44,7 +46,7 @@ def sanitizar_saida(arq, session):
       if (soup.a is not None):
         link =  'https://suap.ifpb.edu.br' + soup.a['href']
         res = session.get(link)
-        valor = pegarDetalhar(res.text)
+        valor = pegar_detalhar(res.text)
 
       else:
         valor = soup.get_text().replace(' ', '').replace('\n', '').replace('\\n', '')
